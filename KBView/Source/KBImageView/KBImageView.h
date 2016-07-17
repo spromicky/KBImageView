67c364a8ee69760ea511a52a7f546a56b4e8be73
spromicky <spromicky@gmail.com>
2016-07-17 20:50:57 +0300

    Complete with documentation.

--- KBView/Source/KBImageView/KBImageView.h
+++ KBView/Source/KBImageView/KBImageView.h
@@ -26,13 +26,40 @@
 
 @interface KBImageView : UIImageView
 
+/**
+ *  Timer that reposive for changing of images.
+ */
 @property (nonatomic, strong)   NSTimer *timer;
+
+/**
+ *  Index of the current selected image in imageView.
+ */
 @property (nonatomic, readonly) NSInteger index;
+
+/**
+ *  What kind of animation is currently is used for image.
+ */
 @property (nonatomic, readonly) NSUInteger animationState;
+
+/**
+ *  What time interval used for every image animation.
+ */
 @property (nonatomic)           IBInspectable CGFloat timePerImage;
+
+/**
+ *  What time interval used for animation changing of images.
+ */
 @property (nonatomic)           IBInspectable CGFloat changeImageTime;
+
+/**
+ *  `YES` if indexes of images are stops to change. And currently just change `animationState` for same image.
+ */
 @property (nonatomic, getter=isIndexPause) BOOL indexPause;
 
+
+/**
+ *  Delegate, that will be used for calls in `KBImageViewDelegate`.
+ */
 @property (nonatomic, weak)     IBOutlet id<KBImageViewDelegate> delegate;
 
 @end
